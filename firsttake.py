97c03bf801e1dcfa385b963d9ed74bf28dd1d7f8
brianthedev <40507013+brianthedev@users.noreply.github.com>
2018-07-05 11:54:55 -0500

    finished the code
    Let me know how i could have coded it better guys. I was excited how fairly easy it was. I know it's a simple one.

--- firsttake.py
+++ firsttake.py
@@ -1,4 +1,12 @@
 def bottles_of_beer(num_of_bottles):
-    while num_of_bottles is > 1:
-        print "There are " + num_of_bottles + " bottles of beer on the wall"
+    while num_of_bottles > 1:
+        print "There are " + str(num_of_bottles) + " bottles of beer on the wall"
+        print str(num_of_bottles) + " bottles of beer"
+        print "You take one down pass it around"
         num_of_bottles -= 1
+        if num_of_bottles == 1:
+            print "There is " + str(num_of_bottles) + " bottle of beer on the wall"
+            print str(num_of_bottles) + " bottle of beer"
+            print "You take one down, you pass it around"
+            print "There are no bottles of beer on the wall"
+print bottles_of_beer(100)
