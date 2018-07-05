2c17c49074b352f90cf4a8f7d7a9aeae6f5a0845
brianthedev <40507013+brianthedev@users.noreply.github.com>
2018-07-05 12:06:21 -0500

    added the space in between every verse

--- firsttake.py
+++ firsttake.py
@@ -3,6 +3,7 @@ def bottles_of_beer(num_of_bottles):
         print "There are " + str(num_of_bottles) + " bottles of beer on the wall"
         print str(num_of_bottles) + " bottles of beer"
         print "You take one down pass it around"
+        print
         num_of_bottles -= 1
         if num_of_bottles == 1:
             print "There is " + str(num_of_bottles) + " bottle of beer on the wall"
